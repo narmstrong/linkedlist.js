4e8a02213f9a276364753751ac36204cf9fbd43a
narmstrong <notnickarmstrong@gmail.com>
2014-04-24 23:37:54 -0400

    Node Methods and Validation Functions

--- linkedlist.js
+++ linkedlist.js
@@ -2,7 +2,7 @@
 // A simple implementation of doubly linked lists in JavaScript
 //
 // Author: Nick Armstrong
-// Version: 0.4
+// Version: 0.5
 //
 // Sources:
 // [1]: http://en.wikipedia.org/wiki/Linked_list
@@ -11,13 +11,13 @@
 // Functions to consider adding:
 //   - Sort Ascending/Descending
 //   - Split
-//   - Mode node in list
+//   - Move node in list
 //   - Shuffle merge
 //   - Shuffle list
 
 
 var LinkedList = {
-  version : 0.1
+  version : 0.5
 }
 
 // Sentinel constructor
@@ -27,15 +27,42 @@ function Sentinel(next, prev) {
 }
 
 // Node constructor
+// TODO Add methods:
+//   -Issupported
 function Node(data, next, prev) {
-
-  if(!arguments.length || data == null)
-    data = {};
-
+  if(!arguments.length)
+    data = null;
   this.data = data;
   Sentinel.prototype.constructor.apply(this, [next, prev]);
   return this;
 }
+
+Node.prototype = {
+
+  // Return the data parameter of the node
+  data : function() {
+    return this.data;
+  },
+
+  // Return the next parameter of the node
+  next : function() {
+    return this.next;
+  },
+
+  // Return the prev parameter of the node
+  prev : function() {
+    return this.prev;
+  },
+
+  // Compare the data, next, and prev elements of two nodes
+  isequal : function(node) {
+    isValidNode(node);
+    return this.data() == node.data() &&
+           this.next() == node.next() &&
+           this.prev() == node.prev();
+  }
+
+};
 
 // List constructor
 function List() {
@@ -59,17 +86,13 @@ List.prototype = {
 
   // Retrieve the nth node in the list
   fetch : function(n) {
-    if(!n || n <= 0 || n > this.length)
-      throw TypeError;
-
+    isInRange.call(this, [n]);
     var node = this.sentinel;
-
     if(n <= this.length / 2) {
       for(var count = 0 ; count < n; count++)
         node = node.next;
       return node;
     }
-
     else {
       for(var count = this.length ; count >= n; count--)
         node = node.prev;
@@ -79,8 +102,8 @@ List.prototype = {
 
   // Insert a new node after the given node
   // TODO insert by number or node
-  // TODO ensure node is valid
   insert : function(node, newnode) {
+    isValidNode(node);
     newnode.next = node.next;
     newnode.prev = node;
     node.next = node.next.prev = newnode;
@@ -89,19 +112,17 @@ List.prototype = {
 
   // Delete a node in the list
   // TODO delete by number or node!
-  // TODO ensure node is valid
   delete : function(node) {
+    isValidNode(node);
     node.prev.next = node.next;
     node.next.prev = node.prev;
-    //TODO set node pointers to the initial node state?
-    // node.next = node;
-    // node.prev = node;
+    node.next = node.prev = node;
     --this.length;
   },
 
   // Add a node with the given data to the end of the list
-  // TODO ensure node is valid
   push : function(node) {
+    isValidNode(node);
     this.insert(this.tail(), node);
   },
 
@@ -114,8 +135,8 @@ List.prototype = {
   },
 
   // Perform fn on each nodes' data in the list
-  // TODO ensure function is valid
   each : function(fn) {
+    isValidFunction(fn);
     var node = this.sentinel;
     while(node.next.hasOwnProperty('data')) {
       fn(node.next.data);
@@ -124,8 +145,8 @@ List.prototype = {
   },
 
   // Perform fn on each node's data in the list and return a new list
-  // TODO ensure function is valid
   map : function(fn) {
+    isValidFunction(fn);
     var list = new List();
     this.each(function(data){
       list.push(new Node(fn(data)));
@@ -145,20 +166,43 @@ List.prototype = {
 
   // Append a copy of a second list to the end of the list
   append : function(list) {
+    isValidList(list);
     if(!list || list.length == 0)
       return this;
-
     var newlist = list.map( function(data){ return data; });
-
     this.tail().next = newlist.head();
     newlist.head().prev = this.tail();
     newlist.tail().next = this.sentinel;
     this.sentinel.prev = newlist.tail();
-
     this.length += list.length;
   }
 
 };
+
+//TODO Validation functions:
+// -isValidData
+// -isInRange
+
+function isInRange(n) {
+  if(!n || n <= 0 || n > this.length)
+    throw TypeError;
+}
+
+function isValidNode(node) {
+  if( !(node instanceof Node) )
+    throw TypeError;
+}
+
+function isValidList(list) {
+  if( !(list instanceof List) )
+    throw TypeError;
+}
+
+function isValidFunction(fn) {
+  if( !(fn instanceof Function) ) {
+    throw TypeError;
+  }
+}
 
 //TODO Print error details to console?
 /*
